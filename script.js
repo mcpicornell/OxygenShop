39835a3ebec316a2cba3f101c5cfd5e3c6f4ed2b
Marc Castro Picornell <120681302+mcpicornell@users.noreply.github.com>
2023-03-05 17:10:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,72 @@ function toggleNav() {
 burger.addEventListener('click', function() {
     toggleNav();
 });
+
+// Scrollbar
+window.onscroll = function onScroll(){
+    let pixelsFromTop = window.scrollY;
+
+    let documentHeight = document.body.clientHeight;
+
+    let windowHeight = window.innerHeight;
+
+    let difference = documentHeight - windowHeight;
+
+    let percentage = (pixelsFromTop *100) / difference;
+
+    document.querySelector(".progress-bar").style.width = `${percentage}%`;
+}
+
+
+// DOM to change the current value in the pricing section
+
+const actualSelection = document.getElementById("selector");
+
+actualSelection.addEventListener("change", async() => {
+
+    let value = actualSelection.value;
+    let symbol = "";
+
+    if (value === "eur") {
+        symbol = "€";
+    } else if (value === "usd") {
+        symbol = "$";
+    } else if (value === "gbp"){
+        symbol = "£";
+    }
+
+    try {
+        let response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${value}.json`);
+
+        if (response.ok) {
+            let data = await response.json();
+
+            let currency = data[value];
+            let basic = Math.round(0 * currency);
+            let pro = Math.round(25 * currency);
+            let premium = Math.round(60 * currency);
+
+            document.getElementById("basic-price").innerHTML = symbol + basic;
+            document.getElementById("pro-price").innerHTML = symbol + pro;
+            document.getElementById("premium-price").innerHTML = symbol + premium;
+
+            
+        } else {
+            console.log("these aren't the droids you're looking for...");
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+/*Vuelve al inicio de la pagina al pulsar el boton*/
+const returnBtn = document.querySelector(".return-button").addEventListener("click", () => {
+    setTimeout(() => {
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 200);
+});
